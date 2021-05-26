12b6691d95becc3d1d69cf712e2f9499313f6f15
Sungpeo Kook <justkook@gmail.com>
2021-05-26 15:24:20 +0900

    test with openjdk 15: result is different

--- run_gc_test.sh
+++ run_gc_test.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-java -verbose:gc -XX:+PrintGCDetails -Xloggc:gc.log.%t -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=10K GcTest
+
+# old java
+$JAVA_HOME/bin/java -verbose:gc -XX:+PrintGCDetails -Xloggc:gc.log.%t -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=3 -XX:GCLogFileSize=10K GcTest
+
+# openjdk 15: create new gc.log with current timestamp everytime
+# java -verbose:gc -Xlog:gc:gc.log.%t GcTest
